8e9dbfad83b25f43f481927397e37d821c973dc4
countgizmo <count.gizmo@gmail.com>
2023-06-30 17:57:39 +0200

    Remove old attempt at decoding

--- 8086/instruction.zig
+++ 8086/instruction.zig
@@ -1,8 +1,3 @@
-pub const registerEncoding = [2][8][]const u8{
-    [_][]const u8{ "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" },
-    [_][]const u8{ "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" },
-};
-
 pub const Register = enum {
     al,
     cl,
