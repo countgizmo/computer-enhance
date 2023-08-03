214534dd3b0a86e9e00e56ccf5a40de57e9b01ad
countgizmo <count.gizmo@gmail.com>
2023-08-03 14:41:24 +0200

    Add quadrants generation with test

--- haversine/input_generator.zig
+++ haversine/input_generator.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const math = std.math;
+const expect = std.testing.expect;
 const fmt = std.fmt;
 const log = std.log;
 const Allocator = std.mem.Allocator;
@@ -12,6 +14,12 @@ const GeneratorError = error {
 const Coordinates = struct {
     x: f64,
     y: f64,
+};
+
+const Quadrant = struct {
+    center: Coordinates,
+    width: u16,
+    height: u16,
 };
 
 const Args = struct {
@@ -56,7 +64,7 @@ fn parseArgs(raw_args: [][:0]u8) !Args {
 fn generateRandomInRange(random: Random, lo: i16, hi: i16) f64 {
     var temp: f64 = random.float(f64);
 
-    return temp + @as(f64, @floatFromInt(random.intRangeAtMost(i16, lo, hi)));
+    return temp + @as(f64, @floatFromInt(random.intRangeAtMost(i16, lo+1, hi-1)));
 }
 
 fn generateCoordinateUniform(random: Random) Coordinates {
@@ -66,6 +74,27 @@ fn generateCoordinateUniform(random: Random) Coordinates {
     };
     return coords;
 }
+
+fn generateQuadrants(random: Random, comptime n: u16, x_start: i16, y_start: i16, x_end: i16, y_end: i16) [n]Quadrant {
+    var quadrants: [n]Quadrant = undefined;
+    const width: u16 = @as(u16, @intCast(@divFloor((x_end - x_start), n)));
+    const height: u16 = @as(u16, @intCast(@divFloor((y_end - y_start), n)));
+    var i: usize = 0;
+
+    while (i < n) : (i += 1) {
+        quadrants[i] = .{
+            .center = .{
+                .x = generateRandomInRange(random, x_start, x_end),
+                .y = generateRandomInRange(random, y_start, y_end),
+            },
+            .width = width,
+            .height = height,
+        };
+    }
+
+    return quadrants;
+}
+
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -116,4 +145,21 @@ pub fn main() !void {
     const avg = sum / @as(f64, @floatFromInt(args.pairs));
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Avg Haversine: {d}\n", .{avg});
+}
+
+test "generating random quadrants" {
+    var prng = std.rand.DefaultPrng.init(12345);
+    const random = prng.random();
+    const quadrants = generateQuadrants(random, 4, -180, -90, 180, 90);
+    log.warn("quadrants = {any}", .{quadrants});
+
+
+    try expect(quadrants.len == 4);
+
+    for (quadrants) |quadrant| {
+        try expect(quadrant.width == 90);
+        try expect(quadrant.height == 45);
+        try expect(quadrant.center.x < 180 and quadrant.center.x > -180 and
+                   quadrant.center.y < 90 and quadrant.center.y > -90);
+    }
 }
