23d0f481751266df819b6af32e0e050112cc5545
countgizmo <count.gizmo@gmail.com>
2023-08-19 11:05:42 +0200

    Playing with binary files

--- haversine/calculator.zig
+++ haversine/calculator.zig
@@ -1,3 +1,7 @@
+const std = @import("std");
+const mem = std.mem;
+
+
 pub const Coordinates = struct {
     x: f64,
     y: f64,
@@ -21,3 +25,24 @@ pub const CoordPair = struct {
         };
     }
 };
+
+test "save coordinates to binary" {
+    const file = try std.fs.cwd().createFile("test.bin", .{});
+    defer file.close();
+
+    const pair = CoordPair.init(10, 20, 30, 40);
+
+    try file.writeAll(&mem.toBytes(pair));
+
+    const file_r = try std.fs.cwd().openFile("test.bin", .{});
+    defer file_r.close();
+
+    var buffer: [1024]u8 = undefined;
+    const read_bytes = try file_r.readAll(&buffer);
+
+    var meaningful_bytes: [32]u8 = undefined;
+    mem.copy(u8, &meaningful_bytes, buffer[0..read_bytes]);
+
+    const pair_r = mem.bytesAsValue(CoordPair, &meaningful_bytes);
+    std.log.warn("{d} - {any}", .{read_bytes, pair_r});
+}
